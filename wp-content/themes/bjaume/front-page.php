8b1042dc67877347e00ed6e3f25389ffdfa3cba0
Benjamin Jaume <47286801+BenjaminJaume@users.noreply.github.com>
2020-03-22 13:32:45 -0600

    Editing front page

--- wp-content/themes/bjaume/front-page.php
+++ wp-content/themes/bjaume/front-page.php
@@ -2,20 +2,178 @@
 
 <?php get_header(); ?>
 
-<div class="container-fluid home-container py-5">
-    <h1 class="text-center">
-        We are Bajo Zero
-    </h1>
-    <p class="text-center">
-        And we are specialised in ice cream rolls on plancha
-    </p>
-    <p class="text-center">
-        <?php echo wp_get_attachment_image(42, '', '', ["class" => "img-front align-middle img-fluid"]); ?>
-    </p>
+<main role="main">
 
+    <div class="container-fluid top-container-front-page py-5">
+        <div class="animated fadeInRight text-center ">
+            <h1 class="text-green text-uppercase font-impact my-0">
+                Bajo Zero
+            </h1>
+            <h3 class="text-pink font-yellowtail mb-0">
+                Heladeria a la plancha
+            </h3>
+            <p class="text-chocolate">Liberia, Costa Rica</p>
+        </div>
 
+        <div class="animated fadeInLeft text-center mb-5">
+            <a href="/bajozero/contact/" class="btn btn-vanilla rounded hvr-sweep-to-top mr-4" alt="">
+                Contact us
+            </a>
 
+            <a href="https://www.facebook.com/BajoZeroCostaRica/" class="btn btn-facebook rounded hvr-sweep-to-bottom" alt="">
+                Follow us on Facebook
+            </a>
+        </div>
 
-</div>
+        <div class="carousel-container mx-auto mb-5">
+            <div id="carousel" class="carousel slide" data-ride="carousel">
+                <ol class="carousel-indicators">
+                    <li data-target="#carousel" data-slide-to="0" class="active"></li>
+                    <li data-target="#carousel" data-slide-to="1"></li>
+                    <li data-target="#carousel" data-slide-to="2"></li>
+                </ol>
+                <div class="carousel-inner rounded">
+                    <div class="carousel-item carousel-item-1 active" data-interval="10000">
+                        <div class="carousel-caption d-sm-block carousel-caption-color-1 text-vanilla">
+                            <h3 class="text-uppercase mb-1"><span>🚀 Discover our fresh products</span></h3>
+                            <p><span>Have a look at our <a href="/bajozero/flavors/" alt="" class="link-green">flavors page</a> to find the ice cream that fits you perfectly</span></p>
+                        </div>
+                    </div>
+                    <div class="carousel-item carousel-item-2" data-interval="10000">
+                        <div class="carousel-caption d-sm-block carousel-caption-color-2 text-vanilla">
+                            <h3 class="text-uppercase mb-1"><span>💬 Follow us on social medias</span></h3>
+                            <p><span>Don't miss any last minute promotions and events at <a href="/bajozero/our-shop/" alt="" class="link-chocolate">our shop</a></span></p>
+                        </div>
+                    </div>
+                    <div class="carousel-item carousel-item-3" data-interval="10000">
+                        <div class="carousel-caption d-sm-block carousel-caption-color-3 text-vanilla">
+                            <h3 class="text-uppercase mb-1"><span>😀 Come and visit us</span></h3>
+                            <p><span>We have a spacious venue with a backyard where you can have fun with your friends!</span></p>
+                        </div>
+                    </div>
+                </div>
+                <a class="carousel-control-prev" href="#carousel" role="button" data-slide="prev">
+                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                    <span class="sr-only">Previous</span>
+                </a>
+                <a class="carousel-control-next" href="#carousel" role="button" data-slide="next">
+                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                    <span class="sr-only">Next</span>
+                </a>
+            </div>
+        </div>
+    </div>
 
-<?php get_footer(); ?>
+    <div class="container my-5">
+        <div class="row featurette">
+            <div class="col-md-7">
+                <h2 class="featurette-heading">This is our team. <span class="text-pink">And we are here for you</span></h2>
+                <p class="lead">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+            </div>
+            <div class="col-md-5">
+                <img src=<?php echo wp_get_attachment_url(103) ?> class="rounded featurette-image img-fluid mx-auto" alt="" width="500" height="500">
+            </div>
+        </div>
+
+        <hr class="featurette-divider mx-auto">
+
+        <div class="row featurette">
+            <div class="col-md-7 order-md-2">
+                <h2 class="featurette-heading">Oh yeah, it’s good. <span class="text-orange">Latest flavor: caramel & vanilla.</span></h2>
+                <p class="lead">Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula porta felis euismod semper. Praesent commodo cursus magna, vel scelerisque nisl consectetur. Fusce dapibus, tellus ac cursus commodo.</p>
+            </div>
+            <div class="col-md-5 order-md-1">
+                <img src=<?php echo wp_get_attachment_url(39) ?> class="rounded featurette-image img-fluid mx-auto" alt="" width="500" height="500">
+
+            </div>
+        </div>
+
+        <hr class="featurette-divider mx-auto">
+
+        <div class="row featurette">
+            <div class="col-md-7">
+                <h2 class="featurette-heading">Come and discover our venue. <span class="text-green">We are located <a href="/bajozero/our-shop/" alt="" class="link-green link-decoration">here</a></span></h2>
+                <p class="lead">Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+            </div>
+            <div class="col-md-5">
+                <img src=<?php echo wp_get_attachment_url(36) ?> class="rounded featurette-image img-fluid mx-auto" alt="" width="500" height="500">
+            </div>
+        </div>
+
+        <hr class="featurette-divider mx-auto">
+    </div>
+
+    <div class="container">
+        <div class="card-deck mb-3 text-center">
+            <div class="card mb-4 shadow-sm">
+                <div class="card-header">
+                    <h4 class="my-0 font-weight-normal">🍦 Ice cream mix</h4>
+                </div>
+                <div class="card-body">
+                    <a href="/bajozero/flavors/" alt="">
+                        <img src=<?php echo wp_get_attachment_url(39) ?> class="rounded img-fluid" alt="" width="500" height="500">
+                    </a>
+
+                    <div class="my-4">
+                        <p class="text-green mb-1">Contains:</p>
+
+                        <ul class="list-unstyled ">
+                            <li>Vanilla</li>
+                            <li>Caramel</li>
+                            <li>Marshmallow</li>
+                            <li>Strawberry topping</li>
+                        </ul>
+                    </div>
+                    <h1 class="card-title pricing-card-title">&#36;3 (&#8353;1500)</h1>
+                    <a href="/bajozero/flavors/" alt="" class="btn btn-lg btn-block btn-vanilla hvr-sweep-to-bottom">Try it</a>
+                </div>
+            </div>
+            <div class="card mb-4 shadow-sm">
+                <div class="card-header">
+                    <h4 class="my-0 font-weight-normal">🍧 Ice cream mix</h4>
+                </div>
+                <div class="card-body">
+                    <a href="/bajozero/flavors/" alt="">
+                        <img src=<?php echo wp_get_attachment_url(34) ?> class="rounded img-fluid" alt="" width="500" height="500">
+                    </a>
+
+                    <div class="my-4">
+                        <p class="text-pink mb-1">Contains:</p>
+
+                        <ul class="list-unstyled ">
+                            <li>Chocolate</li>
+                            <li>Almond</li>
+                            <li>Oreo</li>
+                            <li>Vanilla topping</li>
+                        </ul>
+                    </div>
+                    <h1 class="card-title pricing-card-title">&#36;6 (&#8353;3000)</h1>
+                    <a href="/bajozero/flavors/" alt="" class="btn btn-lg btn-block btn-vanilla hvr-sweep-to-bottom">Try it</a>
+                </div>
+            </div>
+            <div class="card mb-4 shadow-sm">
+                <div class="card-header">
+                    <h4 class="my-0 font-weight-normal">🍨 Ice cream mix</h4>
+                </div>
+                <div class="card-body">
+                    <a href="/bajozero/flavors/" alt="">
+                        <img src=<?php echo wp_get_attachment_url(41) ?> class="rounded img-fluid" alt="" width="500" height="500">
+                    </a>
+
+                    <div class="my-4">
+                        <p class="text-orange mb-1">Contains:</p>
+
+                        <ul class="list-unstyled ">
+                            <li>Pistachio</li>
+                            <li>Mint</li>
+                            <li>No topping</li>
+                        </ul>
+                    </div>
+                    <h1 class="card-title pricing-card-title">&#36;10 (&#8353;5000)</h1>
+                    <a href="/bajozero/flavors/" alt="" class="btn btn-lg btn-block btn-vanilla hvr-sweep-to-bottom">Try it</a>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <?php get_footer(); ?>
