059237cc49991e9b8329ec445d6eed0443e9663c
Benjamin Jaume <47286801+BenjaminJaume@users.noreply.github.com>
2020-03-30 12:39:45 -0600

    Fixing icon

--- wp-content/themes/bajozero/header.php
+++ wp-content/themes/bajozero/header.php
@@ -5,11 +5,10 @@
     <?php wp_head(); ?>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-    <!-- <link rel="apple-touch-icon" sizes="180x180" href="/pictures/favicon/apple-touch-icon.png"> -->
     <link rel="apple-touch-icon" sizes="180x180" href="<?php echo content_url() . '/themes/bajozero/pictures/favicon/apple-touch-icon.png' ?>">
-    <link rel="apple-touch-icon" sizes="180x180" href="<?php echo content_url() . '/themes/bajozero/pictures/favicon/favicon-32x32.png' ?>">
-    <link rel="apple-touch-icon" sizes="180x180" href="<?php echo content_url() . '/themes/bajozero/pictures/favicon/favicon-16x16.png' ?>">
-    <link rel="apple-touch-icon" sizes="180x180" href="<?php echo content_url() . '/themes/bajozero/pictures/favicon/site.webmanifest' ?>">
+    <link rel="icon" type="image/png" sizes="32x32" href="<?php echo content_url() . '/themes/bajozero/pictures/favicon/favicon-32x32.png' ?>">
+    <link rel="icon" type="image/png" sizes="16x16" href="<?php echo content_url() . '/themes/bajozero/pictures/favicon/favicon-16x16.png' ?>">
+    <link rel="manifest" href="<?php echo content_url() . '/themes/bajozero/pictures/favicon/site.webmanifest' ?>">
 
     <?php global $template;
     // console(basename($template)); 
